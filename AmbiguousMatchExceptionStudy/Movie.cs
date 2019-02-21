c03faa6433c837693b0ce1cbcb47c51bc674d023
Edwin <edwinbronyusa@gmail.com>
2019-02-20 22:11:33 -0500

    final checking

---
+++ AmbiguousMatchExceptionStudy/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbiguousMatchExceptionStudy
+{
+    public class Movie
+    {
+        public void DisplayCredits()
+        {
+            Console.WriteLine("Showing All Credits");
+        }
+
+        public void DisplayCredits(int head)
+        {
+            Console.WriteLine($"Showing top {head} Credits");
+        }
+    }
+}
--- AmbiguousMatchExceptionStudy/Program.cs
+++ AmbiguousMatchExceptionStudy/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,29 @@ namespace AmbiguousMatchExceptionStudy
     {
         static void Main(string[] args)
         {
+            //Example3
+            Type movieType = typeof(Movie);
+            var movie = Activator.CreateInstance(movieType);
+
+            MethodInfo DisplayCreditsInfoVoid = movieType.GetMethod("DisplayCredits", new Type[] { });
+            DisplayCreditsInfoVoid.Invoke(movie, null);
+
+            MethodInfo DisplayCreditsInfoParam = movieType.GetMethod("DisplayCredits", new Type[] { typeof(int) });
+            DisplayCreditsInfoParam.Invoke(movie, new object[] {32});
+
+            //Example2
+            //Type movieType = typeof(Movie);
+            //var movie = Activator.CreateInstance(movieType);
+            //MethodInfo DisplayCreditsInfo = movieType.GetMethod("DisplayCredits");
+            //DisplayCreditsInfo.Invoke(movie, null);
+
+
+            //Example1
+            //var movie = new Movie();
+            //movie.DisplayCredits();
+            //movie.DisplayCredits(4);
+
+            Console.ReadKey();
         }
     }
 }
